ed26255cd527a2228ffaad9cdd7b410f5566cd49
James York <james@grandcircus.co>
2016-08-08 17:42:23 -0400

    added simple test to get things started

--- spec/roverSpec.js
+++ spec/roverSpec.js
@@ -1,7 +1,9 @@
 describe("Rover", function() {
+  
   it('should be able to make a rover', function(){
-    var rover = new Rover(0, 0, 'n');
-
-    expect(rover).not.toBeDefined();
+    var rover = new Rover(0, 0, 's');
+    
+    expect(rover).toBeDefined();
   });
+
 });
